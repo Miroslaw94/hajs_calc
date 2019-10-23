36d96894757d01f3e464c6f47950fccd81d382ce
Miroslaw94 <miras_94@wp.pl>
2019-10-23 22:45:19 +0200

    Code refactor.

--- app.js
+++ app.js
@@ -4,32 +4,32 @@ let funnyImage = document.getElementById('image');
 
 function bonusCalculator(money) {
     money = Number(money);
-    let bonus_sum = 0;
-    if (money < 10000) return bonus_sum;
+    let bonusSum = 0;
+    if (money < 10000) return bonusSum;
     money -= 10000;
 
     if (money >= 15000) {
-        bonus_sum += (15000 * 0.05);
+        bonusSum += (15000 * 0.05);
         money -= 15000;
     } else {
-        bonus_sum += (money * 0.05);
+        bonusSum += (money * 0.05);
         money = 0;
     }
 
     let factor = 0.1;
     while (money && factor < 0.3) {
         if (money >= 10000) {
-            bonus_sum += (10000 * factor);
+            bonusSum += (10000 * factor);
             money -= 10000;
         } else {
-            bonus_sum += (money * factor);
+            bonusSum += (money * factor);
             money = 0;
         }
         factor += 0.05;
     }
     console.log(factor);
-    bonus_sum += (money * 0.25);
-    return bonus_sum.toFixed(2);
+    bonusSum += (money * 0.25);
+    return bonusSum.toFixed(2);
 }
 
 function setImage(bonusAmount) {
@@ -44,14 +44,8 @@ function setImage(bonusAmount) {
     }
 }
 
-thresholdInput.addEventListener('input', function () {
-    let justTest = bonusCalculator(thresholdInput.value);
-    bonus.innerHTML = justTest;
-    setImage(justTest);
-});
-
 thresholdInput.addEventListener('keyup', function () {
-    let justTest = bonusCalculator(thresholdInput.value);
-    bonus.innerHTML = justTest;
-    setImage(justTest);
+    let bonusAmount = bonusCalculator(thresholdInput.value);
+    bonus.innerHTML = bonusAmount;
+    setImage(bonusAmount);
 });
